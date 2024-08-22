208e23962e45edc0a6901d4c5dcdcc35889d6b8b
quaziyadgar <quaziyadgar@gmail.com>
2024-08-22 13:36:34 +0530

    project setup is ready

---
+++ hanumanth_vyamshala/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>app</>
+  )
+}
+
+export default App
---
+++ hanumanth_vyamshala/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.scss";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);
